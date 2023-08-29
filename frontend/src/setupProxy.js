efe0553a7dc35a854577dd1271dfdf1aade9b5bc
wayangalihpratama <wgprtm@gmail.com>
2023-08-29 18:43:01 +0800

    [#1] Setup proxy

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,41 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    ["/api/**", "/static/**"],
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    ["/config.js"],
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/config.js": "/api/v1/config.js",
+      },
+    })
+  );
+  app.use(
+    ["/app"],
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/app": "/api/v1/device/apk/download",
+      },
+    })
+  );
+  app.use(
+    ["/i18n.js"],
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/i18n.js": "/api/v1/i18n.js",
+      },
+    })
+  );
+};
