08e15886f75621627461591d166afc7cd5352c69
wayangalihpratama <wgprtm@gmail.com>
2023-08-29 21:49:17 +0800

    [#2] Init core_forms serializers & views

---
+++ backend/akvo/core_forms/serializers.py
@@ -0,0 +1,127 @@
+from collections import OrderedDict
+
+from django.db.models import Q
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field, inline_serializer
+from rest_framework import serializers
+
+from akvo.core_forms.constants import QuestionTypes
+from akvo.core_forms.models import Forms, QuestionGroups, \
+    Questions, Options
+
+
+class ListOptionSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        result = super(ListOptionSerializer, self).to_representation(instance)
+        return OrderedDict([(key, result[key]) for key in result
+                            if result[key] is not None])
+
+    class Meta:
+        model = Options
+        fields = ['id', 'code', 'name', 'order', 'translations']
+
+
+class ListQuestionSerializer(serializers.ModelSerializer):
+    option = serializers.SerializerMethodField()
+    type = serializers.SerializerMethodField()
+    api = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
+    rule = serializers.SerializerMethodField()
+    extra = serializers.SerializerMethodField()
+
+    @extend_schema_field(ListOptionSerializer(many=True))
+    def get_option(self, instance: Questions):
+        if instance.type in [
+            QuestionTypes.option, QuestionTypes.multiple_option
+        ]:
+            return ListOptionSerializer(
+                instance=instance.question_options.all(),
+                many=True
+            ).data
+        return None
+
+    @extend_schema_field(OpenApiTypes.STR)
+    def get_type(self, instance: Questions):
+        return QuestionTypes.FieldStr.get(instance.type).lower()
+
+    @extend_schema_field(
+        inline_serializer(
+            'CascadeApiFormat',
+            fields={
+                'endpoint': serializers.CharField(),
+                'list': serializers.CharField(),
+                'id': serializers.IntegerField(),
+            }
+        )
+    )
+    def get_api(self, instance: Questions):
+        if instance.type == QuestionTypes.cascade:
+            return instance.api
+        return None
+
+    def get_name(self, instance: Questions):
+        return instance.text
+
+    @extend_schema_field(
+        inline_serializer(
+            'QuestionRuleFormat',
+            fields={
+                'min': serializers.FloatField(),
+                'max': serializers.FloatField(),
+                'allowDecimal': serializers.BooleanField(),
+            }
+        )
+    )
+    def get_rule(self, instance: Questions):
+        return instance.rule
+
+    @extend_schema_field(
+        inline_serializer(
+            'QuestionExtraFormat',
+            fields={
+                'allowOther': serializers.BooleanField()
+            }
+        )
+    )
+    def get_extra(self, instance: Questions):
+        return instance.extra
+
+    def to_representation(self, instance):
+        result = super(
+            ListQuestionSerializer, self
+        ).to_representation(instance)
+        return OrderedDict([
+            (key, result[key]) for key in result
+            if result[key] is not None
+        ])
+
+    class Meta:
+        model = Questions
+        fields = [
+            'id', 'name', 'order', 'type', 'required', 'dependency', 'option',
+            'api', 'meta', 'rule', 'extra', 'translations'
+        ]
+
+
+class ListQuestionGroupSerializer(serializers.ModelSerializer):
+    question = serializers.SerializerMethodField()
+
+    @extend_schema_field(ListQuestionSerializer(many=True))
+    def get_question(self, instance: QuestionGroups):
+        return ListQuestionSerializer(
+            instance=instance.question_group_questions.all().order_by('order'),
+            many=True
+        ).data
+
+    class Meta:
+        model = QuestionGroups
+        fields = ['name', 'question', 'translations']
+
+
+class ListFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Forms
+        fields = [
+            'id', 'name', 'description', 'languages', 
+            'version', 'translations'
+        ]
--- backend/akvo/core_forms/views.py
+++ backend/akvo/core_forms/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
 # Create your views here.
+from drf_spectacular.utils import extend_schema
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from akvo.core_forms.models import Forms
+from akvo.core_forms.serializers import ListFormSerializer
+
+
+@extend_schema(
+    responses={
+        200: ListFormSerializer(many=True)
+    },
+    tags=['Form'],
+    summary='To get list of forms',
+    description='Get list of all forms')
+@api_view(['GET'])
+def list_form(request, version):
+    instance = Forms.objects.all()
+    return Response(
+        ListFormSerializer(instance=instance, many=True).data,
+        status=status.HTTP_200_OK
+    )
