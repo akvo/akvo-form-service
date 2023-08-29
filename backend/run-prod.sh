2aeee0ac61c7bfb50b4beac7f30ad27a79faea87
wayangalihpratama <wgprtm@gmail.com>
2023-08-29 13:34:26 +0800

    [#1] Dockerize backend

---
+++ backend/run-prod.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2155
+
+set -eu
+
+# python manage.py migrate
+# python manage.py generate_config >/dev/null &
+# python manage.py generate_views
+# python manage.py generate_sqlite
+# python manage.py collectstatic --no-input
+
+function log {
+  echo "$(date +"%T") - START INFO - $*"
+}
+
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+log Starting gunicorn in background
+gunicorn afs.wsgi --workers 6 --timeout 600 \
+  --bind 0.0.0.0:8000 \
+  --access-logfile ./access.log --error-logfile ./error.log &
+
+child=$!
+wait "$child"
