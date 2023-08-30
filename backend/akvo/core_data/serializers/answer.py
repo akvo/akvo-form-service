f22f4e79906e4eeb610c925a317f5b34598878ac
dedenbangkit <mail@dedenbangkit.com>
2023-08-30 18:20:40 +0700

    [#2] Remove unused import

--- backend/akvo/core_data/serializers/answer.py
+++ backend/akvo/core_data/serializers/answer.py
@@ -1,7 +1,6 @@
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from akvo.core_data.models import Answers
 from akvo.core_forms.constants import QuestionTypes
