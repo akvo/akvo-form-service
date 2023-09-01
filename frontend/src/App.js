793a4257e966368ab0c80dbf19dee45866f274a3
wayangalihpratama <wgprtm@gmail.com>
2023-09-01 09:35:59 +0800

    [#4] Initial ARF PoC form load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { Forms, Form } from "./pages/forms";
 const App = () => {
   return (
     <Routes>
-      <Route exact path="/form/:form_id" element={<Form />} />
+      <Route exact path="/form/:formId" element={<Form />} />
       <Route exact path="/forms" element={<Forms />} />
       <Route exact path="/" element={<Home />} />
     </Routes>
--- frontend/src/pages/forms/Form.js
+++ frontend/src/pages/forms/Form.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "akvo-react-form/dist/index.css"; /* REQUIRED */
+import { Webform } from "akvo-react-form";
+import { api } from "../../lib";
+import { useParams } from "react-router-dom";
 
 const Form = () => {
+  const { formId } = useParams();
+  const [formDef, setFormDef] = useState({});
+
+  useEffect(() => {
+    if (!Object.keys(formDef).length) {
+      api.get(`form/${formId}`).then((res) => {
+        setFormDef(res.data);
+      });
+    }
+  }, [formId, formDef]);
+
+  const onChange = ({ current, values, progress }) => {
+    console.log(progress);
+  };
+
+  const onFinish = (values, refreshForm) => {
+    console.log(values);
+  };
+
   return (
     <div>
-      <h1>Form</h1>
+      {Object.keys(formDef).length > 0 ? (
+        <Webform forms={formDef} onChange={onChange} onFinish={onFinish} />
+      ) : (
+        <h1>Form</h1>
+      )}
     </div>
   );
 };
