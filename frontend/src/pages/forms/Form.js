cff0d061d13203a3f141e1b23dc7cdcf8a004765
wayangalihpratama <wgprtm@gmail.com>
2023-09-01 10:42:11 +0800

    [#4] Handle onFinish ARF event

--- frontend/src/pages/forms/Form.js
+++ frontend/src/pages/forms/Form.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import "akvo-react-form/dist/index.css"; /* REQUIRED */
 import { Webform } from "akvo-react-form";
 import { api } from "../../lib";
@@ -16,24 +16,47 @@ const Form = () => {
     }
   }, [formId, formDef]);
 
+  const questions = useMemo(() => {
+    if (!Object.keys(formDef).length) {
+      return [];
+    }
+    return formDef.question_group.flatMap((qg) => qg.question);
+  }, [formDef]);
+
   const onChange = ({ current, values, progress }) => {
     console.log(progress);
   };
 
   const onFinish = (values, refreshForm) => {
-    let payload = { data: { ...values.datapoint, submitter: 'Akvo'}}
-    const answers = Object.keys(values).map(key => {
-      if (key === "datapoint") {
-        return false;
-      }
-      const value = values[key]
-      return {
-        question: parseInt(key),
-        value: value
-      }
-    }).filter(x => x)
-    payload = {...payload, answer: answers}
-    console.log(payload);
+    let payload = { data: { ...values.datapoint, submitter: "Akvo" } };
+    const answers = Object.keys(values)
+      .map((key) => {
+        if (key === "datapoint") {
+          return false;
+        }
+        // remap answer by question type
+        const qid = parseInt(key);
+        const question = questions.find((q) => q.id === qid);
+        let value = values[key];
+        if (question.type === "option") {
+          value = [value];
+        }
+        return {
+          question: qid,
+          value: value,
+        };
+      })
+      .filter((x) => x);
+    payload = { ...payload, answer: answers };
+    api
+      .post(`data/${formId}`, payload)
+      .then(() => {
+        console.info("Submitted");
+        refreshForm();
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   };
 
   return (
