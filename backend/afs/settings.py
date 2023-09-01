6d4f79fb79589c69ac0c6df65ed59fe99926bb44
dedenbangkit <mail@dedenbangkit.com>
2023-09-01 07:35:07 +0700

    [#4] Folder structure, move static, add cli app

--- backend/afs/settings.py
+++ backend/afs/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cli",
     "akvo.core_forms",
     "akvo.core_node",
     "akvo.core_data",
@@ -142,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static-files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ frontend/src/pages/forms/Forms.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Forms = () => {
+	return (
+		<div>
+			<h1>Form</h1>
+		</div>
+}
+
+export default Forms;
