960289845022466d74281180d67905c2aa935b35
dedenbangkit <mail@dedenbangkit.com>
2023-09-01 09:31:00 +0700

    [#5] Fix package.json modify index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "antd/dist/antd.min.css";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
