1371df0b21a5046b2e5504d6901e401970c92661
dedenbangkit <mail@dedenbangkit.com>
2023-09-06 21:05:34 +0700

    [#15] Fix doom element id

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const container = document.getElementById("app");
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
